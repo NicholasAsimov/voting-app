c6857517443dcbaada6773bf5b5b2f19c8ee014f
Nicholas Asimov <kolyaazimov@gmail.com>
2016-04-18 22:31:48 +0300

    Initial boilerplate

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
-'use strict';
+const express = require('express');
+const routes = require('./app/routes/index.js');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
 
-var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
-var app = express();
+const app = express();
 require('dotenv').load();
 require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+app.use('/controllers', express.static(`${process.cwd()}/app/controllers`));
+app.use('/public', express.static(`${process.cwd()}/public`));
+app.use('/common', express.static(`${process.cwd()}/app/common`));
 
 app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
+  secret: 'secretClementine',
+  resave: false,
+  saveUninitialized: true
 }));
 
 app.use(passport.initialize());
@@ -27,7 +25,7 @@ app.use(passport.session());
 
 routes(app, passport);
 
-var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Node.js listening on port ${port}...`);
 });
